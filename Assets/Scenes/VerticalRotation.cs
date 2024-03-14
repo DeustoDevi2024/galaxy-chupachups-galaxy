c076cfa2634cba546116b40d81ec09b398e29ae8
Jaivaan <ivan.renteria@opendeusto.es>
2024-03-14 08:51:37 +0100

    vertical rotation

---
+++ Assets/Scenes/VerticalRotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speedRotation = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(speedRotation* Time.deltaTime * 7, 0f, 0f);
+    }
+}
